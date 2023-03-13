7420be0652d7e5ad06047944d4cc040ce5f985a9
alonso523 <alonso523@hotmail.com>
2023-03-13 14:41:57 -0600

    Badge 2 Snowflake
    New Changes 2023

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rocket Smothie')
+streamlit.header('Hard-Boiled Free-Range Egg')
+streamlit.header('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
