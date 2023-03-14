39b485a1ee89f4a5377349f0eb20fb3fe12fbf64
alonso523 <alonso523@hotmail.com>
2023-03-14 11:05:40 -0600

    new identation fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,8 @@ except URLError as e:
 	if streamlit.button('Get Fruit Load List'):
 		my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 		my_data_rows = get_fruit_load_list()
-	my_cur.execute("SELECT * from fruit_load_list") 
-	streamlit.dataframe(my_data_rows)
-	# Allow the end user to add a fruit to the list
-	streamlit.write('Thanks for adding ', add_my_fruit)
-	my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+		streamlit.dataframe(my_data_rows)
+		# Allow the end user to add a fruit to the list
+		streamlit.write('Thanks for adding ', add_my_fruit)
+		my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
