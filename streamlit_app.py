3275bd84315b6280a3ea95dc8fea6f743986ffb1
alonso523 <alonso523@hotmail.com>
2023-03-14 10:36:58 -0600

    borrar identación

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,8 @@ try:
 	else:
 		back_from_function = get_fruityvice_data(fruit_choice)
 		streamlit.dataframe(back_from_function)
-		
 #don´t run anything past here while we troubleshoot
-	streamlit.stop()
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
