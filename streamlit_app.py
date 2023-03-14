00743106235d614f166a3fa1683704175e74190c
alonso523 <alonso523@hotmail.com>
2023-03-14 10:34:01 -0600

    identation fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ streamlit.dataframe(fruits_to_show)
 
 # Creaete the respeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-		return fruityvice_normalized
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
@@ -40,7 +40,6 @@ try:
 		back_from_function = get_fruityvice_data(fruit_choice)
 		streamlit.dataframe(back_from_function)
 		
-except URLError e:
 #don´t run anything past here while we troubleshoot
 streamlit.stop()
 
