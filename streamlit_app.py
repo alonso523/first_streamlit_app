b7dea6ed9b69a699c869ba8bbbe72ab678372f09
alonso523 <alonso523@hotmail.com>
2023-03-14 10:46:37 -0600

    identation #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,16 +41,16 @@ try:
 		streamlit.dataframe(back_from_function)
 except URLError as e:
 #don´t run anything past here while we troubleshoot
-streamlit.stop()
+	streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	my_cur = my_cnx.cursor()
+	my_cur.execute("SELECT * from fruit_load_list")
+	my_data_rows = my_cur.fetchall()
+	streamlit.header("The fruit load list contains")
+	streamlit.dataframe(my_data_rows)
 
-# Allow the end user to add a fruit to the list
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+	# Allow the end user to add a fruit to the list
+	streamlit.write('Thanks for adding ', add_my_fruit)
+	my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
